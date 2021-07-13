cab99c59af35e4c71a45f86d518c7a0713b6d081
yewang <yewang@baidu.com>
2021-07-13 20:57:20 +0800

    rebuilding site Tue Jul 13 20:57:19 CST 2021

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -x
-printf "\033[0;32m apps Deploying updates to GitHub...\033[0m\n"
+printf "\033[0;32m app Deploying updates to GitHub...\033[0m\n"
 cd $(dirname "$0")
 
 # Commit changes.
@@ -14,7 +14,16 @@ rm -rf public/*
 # Build the project.
 hugo
 
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+git commit -m "$msg"
+# Push to blog repo and trigger building blog.
+git push origin master
+
 # push to source repo
+cd ..
 git add .
 git commit -m "$msg"
 git push origin master
